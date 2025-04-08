a649bf4c533b259d9ad46830abe52b8082eb3a6d
Let�cia Oliveira dos Santos <43705577808@>
2025-04-08 10:58:58 -0300

    user2

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(unique=True)
+    phone = models.CharField(max_length=10, unique=True)
     address = models.TextField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     is_verified = models.BooleanField(default=False)
