a81342bdaf2f739547925c226694231d95f434cd
Let�cia Oliveira dos Santos <43705577808@>
2025-04-08 11:16:40 -0300

    user cadastro

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ('username', 'phone', 'address', 'birth_date', 'is_verified',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone', 'address', 'birth_date', 'is_verified')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone', 'address', 'birth_date', 'is_verified')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+@api_view(['POST'])
+def criar_usuario(request):
+    username = request.data.get('username')
+    senha = request.data.get('senha')
+    phone = request.data.get('phone')
+    address = request.data.get('address')
+    birth_date = request.data.get('birth_date')
+    is_verified = request.data.get('verified')
+
+    if not username or not senha or not phone:
+        return Response({'Erro': 'Campos obrigatórios incompletos'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if CustomUser.objects.filter(username=username).exists():
+        return Response({'Erro': f'Username {username} já existe'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if CustomUser.objects.filter(phone=phone).exists():
+        return Response({'Erros': f'telefone {phone} já existe'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    usuario = CustomUser.objects.create_user(
+        username=username,
+        password=senha,
+        phone=phone,
+        address=address,
+        birth_date=birth_date,
+        is_verified=is_verified
+    )
+    return Response({'Mensagem': f'Usuário {username} criado com sucesso'}, status=status.HTTP_201_CREATED)
